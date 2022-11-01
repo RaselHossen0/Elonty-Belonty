42076794f2d75d37b26994fbc37b6ffec16f3304
MD. RASEL HOSSEN <hrasel2002@gmail.com>
2022-11-01 10:19:23 +0600

    jhhgkj

--- loadTex.h
+++ loadTex.h
@@ -1,14 +1,59 @@
 #pragma once
 #include "header.h"
 SDL_Texture *bg222;
-
-
+ SDL_Texture *replay_tex,*tex0;
+ SDL_Rect replay_rect;
 void texCreate(){
+
+     surface = IMG_Load("res/dip.jpg");
+    if (!surface)
+    {
+        printf("Redbar Surface Error: %s\n", IMG_GetError());
+        SDL_DestroyRenderer(rend);
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+      //  return 1;
+    }
+    tex0 = SDL_CreateTextureFromSurface(rend, surface);
+    SDL_FreeSurface(surface);
+    if (!tex0)
+    {
+        printf("Redline Texture %s\n", SDL_GetError());
+        SDL_DestroyRenderer(rend);
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+      //  return 1;
+    }
+
     surface=IMG_Load("res/bg.jpg");
     bg222=SDL_CreateTextureFromSurface(rend,surface);
     SDL_FreeSurface(surface);
 
-    surface=IMG_Load("")
+    surface=IMG_Load("");
     extern SDL_Texture *tryA;
 
+    surface = IMG_Load("res/replay.png");
+    if (!surface)
+    {
+        printf("replay Surface Error: %s\n", IMG_GetError());
+        SDL_DestroyRenderer(rend);
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+       // return 1;
+    }
+   replay_tex = SDL_CreateTextureFromSurface(rend, surface);
+    SDL_FreeSurface(surface);
+    if (!replay_tex)
+    {
+        printf("replayTexture %s\n", SDL_GetError());
+        SDL_DestroyRenderer(rend);
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+        //return 1;
+    }
+ 
+    replay_rect.w = 200;
+    replay_rect.h = 100;
+    replay_rect.x = (WINDOW_WIDTH - replay_rect.w) / 2;
+    replay_rect.y = (WINDOW_HEIGHT - replay_rect.h) / 2;
 }
